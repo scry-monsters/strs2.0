8c3bed23e5e1f3436f04386a0ebfe39873031099
Aziz Kurbanov <krbnv.z@gmail.com>
2022-08-12 02:00:10 +0600

    *

---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,131 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
+
+type Anchor = "left";
+interface Item {
+	id: number;
+	icon: HTMLImageElement;
+	url: string;
+	text: string;
+}
+
+let socialMedia = [
+	{
+		id: 0,
+		icon: LinkedInIcon,
+		url: "https://www.linkedin.com/in/scry/",
+		text: "LinkedIn",
+	},
+
+	{
+		id: 1,
+		icon: GitHubIcon,
+		url: "https://github.com/scry-monsters",
+		text: "GitHub",
+	},
+	{
+		id: 2,
+		icon: MarkEmailReadIcon,
+		url: "mailto:krbnv.z@gmail.com",
+		text: "Gmail",
+	},
+];
+
+export default function Burger() {
+	const [state, setState] = React.useState({
+		left: false,
+	});
+
+	const toggleDrawer =
+		(anchor: Anchor, open: boolean) =>
+		(event: React.KeyboardEvent | React.MouseEvent) => {
+			if (
+				event.type === "keydown" &&
+				((event as React.KeyboardEvent).key === "Tab" ||
+					(event as React.KeyboardEvent).key === "Shift")
+			) {
+				return;
+			}
+
+			setState({ ...state, [anchor]: open });
+		};
+
+	const list = (anchor: Anchor) => (
+		<Box
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<List>
+				{socialMedia.map((item) => (
+					<a href={item.url}>
+						<ListItem key={item.id} disablePadding>
+							<ListItemButton>
+								<ListItemIcon>
+									<item.icon />
+								</ListItemIcon>
+								<ListItemText primary={item.text} />
+							</ListItemButton>
+						</ListItem>
+					</a>
+				))}
+			</List>
+			<Divider />
+			{/* <List>
+				{["LinkedIn", "Github", "Source code", "Insta"].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+							<ListItemIcon>
+								{index === 0 ? "icons go here" : "same here"}
+							</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List> */}
+		</Box>
+	);
+
+	return (
+		<div>
+			{(["left"] as const).map((anchor) => (
+				<React.Fragment key={anchor}>
+					<Button onClick={toggleDrawer(anchor, true)}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							strokeWidth={2}
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M4 6h16M4 12h16M4 18h16"
+							/>
+						</svg>
+					</Button>
+					<Drawer
+						anchor={anchor}
+						open={state[anchor]}
+						onClose={toggleDrawer(anchor, false)}
+					>
+						{list(anchor)}
+					</Drawer>
+				</React.Fragment>
+			))}
+		</div>
+	);
+}
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import classes from "./NavBar.module.scss";
 import classNames from "classnames";
 import myLogo from "../../images/myLogo.png";
+import Burger from "../Burger/Burger";
 const NavBar = () => {
 	return (
-		<nav className={classNames(classes.main_navbar, "relative mx-auto")}>
+		<nav
+			className={classNames(
+				classes.main_navbar,
+				"relative grid  grid-cols-3 mx-auto"
+			)}
+		>
+			<div className="pl-1 pt-2">
+				<Burger />
+			</div>
 			<div
 				className={classNames(
 					classes.image_container,
